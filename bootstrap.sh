c505b1303bd8fbdfb1bd630892601b2b339dcdcc
Babacar Gueye <babacar.gueye@epitech.eu>
2014-04-23 10:52:35 +0200

    bootstrap script to rule them all.

---
+++ bootstrap.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+## use zsh after
+
+cd "$(dirname "${BASH_SOURCE}")"
+git pull origin master
+
+function doIt() {
+    
+    # change files to .something and use it
+
+    rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
+	--exclude "README.md" --exclude "LICENSE-MIT.txt" -av --no-perms . ~
+    source ~/.bash_profile
+    # source ~/.zshrc
+}
+
+if [ "$1" == "--force" -o "$1" == "-f" ]; then
+    doIt
+else
+    read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
+    echo
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+	doIt
+    fi
+fi
+unset doIt
